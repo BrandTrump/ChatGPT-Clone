2246ff9c7c624730fc1dda5237c5be413e60d028
Brandon Trump <brandontrump101@gmail.com>
2023-04-05 12:14:02 +1200

    included model selection and previous chats

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,19 +1,46 @@
 "use client";
 import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import { collection, query, orderBy } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "@/firebase";
+import ChatRow from "./ChatRow";
+import ModelSelection from "./ModelSelection";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
           <NewChat />
-          <div>{/*Model Selection*/}</div>
+          <div className="hidden sm:inline">
+            <ModelSelection />
+          </div>
+
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
       </div>
 
       {session && (
+        // eslint-disable-next-line @next/next/no-img-element
         <img
           onClick={() => signOut()}
           src={session.user?.image!}
